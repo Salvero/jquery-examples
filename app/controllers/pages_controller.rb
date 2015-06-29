cc410f97bf8621510edfe569a38e64dc8edab85f
Salman Akram <thunder9ruby@gmail.com>
2015-06-29 19:25:05 -0400

    create jquery elements

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,4 @@
+class PagesController < ApplicationController
+	def character
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  
+
   get 'welcome/index'
+  get '/character' => 'pages#character'
 
   root 'welcome#index'
 
